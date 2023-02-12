cd8a1ec78cd62d5676f17f67ff76d330e9df34ac
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-12 13:33:39 -0300

    Implementação das funções: reset, renderSquare e renderInfo

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Initial data (Dados iniciais)
+let square = {
+  a1: "",
+  a2: "",
+  a3: "",
+  b1: "",
+  b2: "",
+  b3: "",
+  c1: "",
+  c2: "",
+  c3: "",
+};
+
+// Time to play (Vez de jogada)
+let player = "";
+// Winner's message - (Mensagem do vencedor)
+let warning = "";
+// Check if the game is running (Verificar se o jogo está rolando ou não)
+let playing = false;
+reset();
+// Events (Eventos)
+document.querySelector(".reset").addEventListener("click", reset);
+
+// Functions (Funções)
+
+function reset() {
+  //clearing the warnings (Limpando avisos)
+  warning = "";
+
+  //Randomly choosing time(escolhendo a vez aleatoriamente)
+  let random = Math.floor(Math.random() * 2);
+  player = random === 0 ? "x" : "o";
+
+  //clearing the plays (Limpando as jogadas)
+  for (let i in square) {
+    square[i] = "";
+  }
+  playing = true;
+
+  renderSquare();
+  renderInfo();
+}
+
+function renderSquare() {
+  for (let i in square) {
+    let item = document.querySelector(`div[data-item=${i}]`);
+    item.innerHTML = square[i];
+  }
+}
+function renderInfo() {
+   document.querySelector('.vez').innerHTML = player;
+   document.querySelector('.resultado').innerHTML = warning;
+}
