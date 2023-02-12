d7194d114d14bcb363367fe275464eab3ba11bd2
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-12 14:24:11 -0300

    Finalizado a lógica do vencedor

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,7 @@ document.querySelectorAll(".item").forEach((item) => {
 // Functions (Funções)
 function itemClick(event) {
   let item = event.target.getAttribute("data-item");
-  if (square[item] === "") {
+  if (playing && square[item] === "") {
     square[item] = player;
     renderSquare();
     togglePlayer();
@@ -57,6 +57,7 @@ function renderSquare() {
     let item = document.querySelector(`div[data-item=${i}]`);
     item.innerHTML = square[i];
   }
+  checkGame();
 }
 function renderInfo() {
   document.querySelector(".vez").innerHTML = player;
@@ -65,4 +66,45 @@ function renderInfo() {
 function togglePlayer() {
   player = player === "x" ? "o" : "x";
   renderInfo();
+}
+function checkGame(){
+   if(checkInnerFor('x')){
+      warning = "O 'x' venceu!"
+      playing = false;
+   }else if(checkInnerFor('o')){
+      warning = "O 'o' venceu!"
+      playing = false;
+   }else if(ifFull()){
+      warning = "Deu impate!"
+      playing = false;
+   }
+}
+function checkInnerFor(player){
+   let possibilities = [
+      'a1,a2,a3',
+      'b1,b2,b3',
+      'c1,c2,c3',
+      'a1,b1,c1',
+      'a2,b2.c2',
+      'a3,b3,c3',
+      'a1,b2,c3',
+      'a3,b2,c1'
+   ];
+
+   for(let w in possibilities){
+      let pArray = possibilities[w].split(',');
+      let hasWon = pArray.every(option => square[option] === player);
+      if(hasWon){
+         return true;
+      }
+   }
+   return false;
+}
+function ifFull(){
+   for(let i in square){
+      if(square[i] === ''){
+         return false;
+      }
+   }
+   return true;
 }
